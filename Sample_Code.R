ed92e386f80967c039f935236383be566a8a8348
Sarvgya <sarvgyakumar@gmail.com>
2021-05-24 16:27:05 +0530

    code part 1

---
+++ Sample_Code.R
@@ -0,0 +1,4 @@
+# Investigation the exponential distribution in R and compare it with the Central Limit Theorem.
+# Aim: To investigate the distribution of averages of 40 exponential.
+# Simulate this exponential distribution 1000 times.
+
